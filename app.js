52cdb9feb89a7ae1d1c26dee1dd1b1436d1493b0
MartaZare <marta.zarec@gmail.com>
2023-04-06 09:48:51 +0300

    Added reading status buttons

--- app.js
+++ app.js
@@ -5,14 +5,59 @@ const fauthor = document.getElementById("fauthor");
 const ftitle = document.getElementById("ftitle");
 const fpages = document.getElementById("fpages");
 
-function Book(title, author, pages) {
-  (this.title = title), (this.author = author), (this.pages = pages);
+function Book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
 }
 
-function displayList(size) {
-  for (let i = 0; i < size; i++) {
+function displayForm(e) {
+  if (e == "on") {
+    document.getElementById("bookForm").style.display = "block";
+    document.getElementById("addBookBtn").style.display = "none";
+  } else {
+    document.getElementById("bookForm").style.display = "none";
+    document.getElementById("addBookBtn").style.display = "block";
+  }
+}
+
+function addBook() {
+  submitForm();
+  reloadForm();
+  displayShelf();
+}
+
+function displayShelf() {
+  clearShelf();
+  loadShelf();
+}
+
+function submitForm() {
+  list.push(
+    new Book(
+      document.getElementById("ftitle").value,
+      document.getElementById("fauthor").value,
+      document.getElementById("fpages").value
+    )
+  );
+}
+
+function reloadForm() {
+  ftitle.value = "";
+  fauthor.value = "";
+  fpages.value = "";
+}
+
+function clearShelf() {
+  shelf.innerHTML = "";
+}
+
+function loadShelf() {
+  for (let i = 0; i < list.length; i++) {
     const oneBook = document.createElement("div");
     oneBook.classList.add("bookCard");
+    oneBook.classList.add(i);
     shelf.appendChild(oneBook);
 
     const title = document.createElement("div");
@@ -29,36 +74,64 @@ function displayList(size) {
     pages.classList.add("pages");
     pages.innerHTML += list[i].pages;
     oneBook.appendChild(pages);
+    removeBookBtn(oneBook);
+    readingStatusBtn(oneBook);
   }
 }
 
-function displayForm(e) {
-  if (e == "on") {
-    document.getElementById("bookForm").style.display = "block";
-  } else {
-    document.getElementById("bookForm").style.display = "none";
+function removeBookBtn(parentElement) {
+  const remove = document.createElement("button");
+  remove.classList.add("remove");
+  remove.innerHTML = "Remove";
+  parentElement.appendChild(remove);
+
+  remove.addEventListener("click", function (e) {
+    list.splice(e.target.parentElement.classList.item(1), 1);
+    displayShelf();
+  });
+}
+
+function readingStatusBtn(parentElement) {
+  const read = document.createElement("button");
+  read.classList.add("read");
+  read.innerHTML = "Read";
+  parentElement.appendChild(read);
+  read.onclick = () => readingStatusColor(read);
+
+  const reading = document.createElement("button");
+  reading.classList.add("reading");
+  reading.innerHTML = "Reading";
+  parentElement.appendChild(reading);
+  reading.onclick = () => readingStatusColor(reading);
+
+  const notread = document.createElement("button");
+  notread.classList.add("notread");
+  notread.innerHTML = "Not read";
+  parentElement.appendChild(notread);
+  notread.onclick = () => readingStatusColor(notread);
+}
+
+function readingStatusColor(status) {
+  deactivateAllBtn();
+  status.classList.add("active");
+  const activeButton = document.querySelector(".active");
+  if (activeButton.classList.contains("reading")) {
+    activeButton.style.backgroundColor = "yellow";
+  } else if (activeButton.classList.contains("read")) {
+    activeButton.style.backgroundColor = "green";
+  } else if (activeButton.classList.contains("notread")) {
+    activeButton.style.backgroundColor = "blue";
   }
 }
 
-function submitForm() {
-  document.getElementById("form1");
-  console.log("form subbmited)");
-  addBook();
-  reloadForm();
+function deactivateAllBtn() {
+  const activeBtn = document.querySelectorAll(".active");
+  [].forEach.call(activeBtn, function (e) {
+    setDefaultBtnColor(e);
+    e.classList.remove("active");
+  });
 }
 
-function addBook() {
-  list.push(
-    new Book(
-      document.getElementById("ftitle").value,
-      document.getElementById("fauthor").value,
-      document.getElementById("fpages").value
-    )
-  );
-}
-
-function reloadForm() {
-  ftitle.value = "";
-  fauthor.value = "";
-  fpages.value = "";
+function setDefaultBtnColor(e) {
+  e.style.backgroundColor = "white";
 }
